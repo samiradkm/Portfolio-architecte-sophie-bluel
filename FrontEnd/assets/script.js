7664ca456d900e841f73d25f0524d23446d74a35
Samira SALIFOU MOHAMED <samirasalifoumohamed@gmail.com>
2025-02-25 01:21:53 +0100

    fin de l'etape 2

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,6 @@
 console.log("bonjour samira");
 
+let loginToken = null;
 let works = [];
 const gallery = document.querySelector('section#portfolio div.gallery');
 const categories = document.querySelector('section#portfolio div.categories');
@@ -85,5 +86,33 @@ function desactiveFilters() {
     for (let i = 0; i < buttonFilters.length; i++) {
         buttonFilters[i].classList.remove("active");
     }
-
 }
+
+document.getElementById('login-form').addEventListener('submit', (e) => {
+    e.preventDefault(); // Blocage du rechargement de la page
+
+    const formData = new FormData(e.target);
+    const jsonData = Object.fromEntries(formData.entries()); // Convertir FormData en objet JSON
+
+    fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Indiquer qu'on envoie du JSON
+        },
+        body: JSON.stringify(jsonData)
+    })
+        .then((response) => {
+            console.log(response);
+            if (response.status !== 200) {
+                response.json().then((data) => {
+                    document.querySelector('.error').textContent = data.message;
+                });
+            } else {
+
+                response.json().then((data) => {
+                    loginToken = data.token;
+                    window.location.href = 'index.html';
+                });
+            }
+        })
+})
